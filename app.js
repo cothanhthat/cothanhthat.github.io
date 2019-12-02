c453b6ae61d326438b727bcf274a0eeffa9679e9
cothanhthat <yen.m.t.trinh@ntnu.no>
2019-12-02 20:34:04 +0100

    hi Van

--- app.js
+++ app.js
@@ -93,13 +93,13 @@ var index = 0;
 (function(){
     // Your web app's Firebase configuration
     var config = {
-        apiKey: "AIzaSyDuMfH66rv7R_A3JKhstTanRlOCFFRjXKE",
-        authDomain: "surveytest-ec05d.firebaseapp.com",
-        databaseURL: "https://surveytest-ec05d.firebaseio.com",
-        projectId: "surveytest-ec05d",
-        storageBucket: "surveytest-ec05d.appspot.com",
-        messagingSenderId: "842946839037",
-        appId: "1:842946839037:web:eddfc5fee601547a1f9994"
+        apiKey: "AIzaSyCGxeeURfi-mMXDNzCm3dxgBK9j-uj_WU8",
+    authDomain: "cothanhthat.firebaseapp.com",
+    databaseURL: "https://cothanhthat.firebaseio.com",
+    projectId: "cothanhthat",
+    storageBucket: "cothanhthat.appspot.com",
+    messagingSenderId: "99919601515",
+    appId: "1:99919601515:web:0b5b33a98162e5753d762e"
     };
 
     // Initialize Firebase
